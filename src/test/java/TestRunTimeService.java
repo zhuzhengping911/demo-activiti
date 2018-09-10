5661d5e5649ff91f6ba041e59b2153b1740fe8a6
zhuzhengping911 <zhu_zhengping@hotmail.com>
2018-09-11 01:28:01 +0800

    Test RunTimeService

---
+++ src/test/java/TestRunTimeService.java
@@ -0,0 +1,92 @@
+import com.google.common.collect.Maps;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.runtime.ProcessInstanceBuilder;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * RunTimeService Test
+ * Created by zhuzhengping
+ * on 2018/9/10.
+ */
+public class TestRunTimeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestRunTimeService.class);
+
+    @Rule
+    public ActivitiRule activitiRule = new ActivitiRule();
+
+    @Test
+    @org.activiti.engine.test.Deployment(resources = "MyProcess.bpmn20.xml")
+    public void testStartProcess(){
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        Map<String,Object> varibles = Maps.newHashMap();
+        varibles.put("key1","value1");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess",varibles);
+        LOGGER.info("processInstance = {}",processInstance);
+    }
+
+    @Test
+    @org.activiti.engine.test.Deployment(resources = "MyProcess.bpmn20.xml")
+    public void testStartProcessById(){
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        ProcessDefinition processDefinition = activitiRule.getRepositoryService().createProcessDefinitionQuery().singleResult();
+
+        Map<String,Object> varibles = Maps.newHashMap();
+        varibles.put("key1","value1");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId(),varibles);
+        LOGGER.info("processInstance = {}",processInstance);
+    }
+
+    @Test
+    @org.activiti.engine.test.Deployment(resources = "MyProcess.bpmn20.xml")
+    public void testStartProcessInstanceBuilder(){
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        Map<String,Object> varibles = Maps.newHashMap();
+        varibles.put("key1","value1");
+        ProcessInstanceBuilder processInstanceBuilder = runtimeService.createProcessInstanceBuilder();
+        ProcessInstance processInstance = processInstanceBuilder.businessKey("business001")
+                .processDefinitionKey("myProcess")
+                .variables(varibles)
+                .start();
+        LOGGER.info("processInstance = {}",processInstance);
+    }
+
+
+    @Test
+    @org.activiti.engine.test.Deployment(resources = "MyProcess.bpmn20.xml")
+    public void testVariables(){
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        Map<String,Object> varibles = Maps.newHashMap();
+        varibles.put("key1","value1");
+        varibles.put("key2","value2");
+        varibles.put("key3","value3");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess",varibles);
+        LOGGER.info("processInstance = {}",processInstance);
+        runtimeService.setVariable(processInstance.getId(),"key3","123");
+        runtimeService.setVariable(processInstance.getId(),"key2","321");
+        Map<String, Object> variables = runtimeService.getVariables(processInstance.getId());
+        LOGGER.info("variables = {}",variables);
+    }
+
+    @Test
+    @org.activiti.engine.test.Deployment(resources = "MyProcess.bpmn20.xml")
+    public void testProcessInstanceQuery(){
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        Map<String,Object> varibles = Maps.newHashMap();
+        varibles.put("key1","value1");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess",varibles);
+        LOGGER.info("processInstance = {}",processInstance);
+
+        ProcessInstance processInstance1 = runtimeService.createProcessInstanceQuery()
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+    }
+}
