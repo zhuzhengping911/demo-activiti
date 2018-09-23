436d80bf96054078d25b38086dc45c22e7696235
zhuzhengping911 <zhu_zhengping@hotmail.com>
2018-09-24 03:58:25 +0800

    SpringBoot moudle

--- springboot/src/test/java/com/zzp/activiti/springboot/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/zzp/activiti/springboot/springboot/SpringbootApplicationTests.java
@@ -1,16 +1,29 @@
 package com.zzp.activiti.springboot.springboot;
 
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.test.ActivitiRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
+    Logger logger = LoggerFactory.getLogger(SpringbootApplicationTests.class);
+
+    @Autowired
+    public RuntimeService runtimeService;
 
     @Test
     public void contextLoads() {
+        ProcessInstance myProcess = runtimeService.startProcessInstanceByKey("myProcess");
+        logger.info(" this is the instance = {}",myProcess);
+
     }
 
 }
